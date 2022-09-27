bf0087459cf1f5a0bc3194fc538e85060075610d
SYLLAS ANTONIO BARBOSA BRAGA <0055818@IFMG.EDU.BR>
2022-09-27 16:02:31 -0300

    Implementando get direto da API

--- Visual/assets/script/Administradores.js
+++ Visual/assets/script/Administradores.js
@@ -1,5 +1,14 @@
+const table = document.getElementById("adms-row");
+
 const showData = (result) => {
   for(const campo in result){
+    table.innerHTML = "<td> " + result[campo].nome + "</td>";
+    table.innerHTML = "<td> " + result[campo].sobrenome + "</td>";
+    table.innerHTML = "<td> " + result[campo].login + "</td>";
+    table.innerHTML = "<td> " + result[campo].cpf + "</td>";
+    table.innerHTML = "<td> " + result[campo].salario + "</td>";
+    table.innerHTML = "<td> " + result[campo].dataNascimento + "</td>";
+
     console.log (result[campo]);
   }
 }
