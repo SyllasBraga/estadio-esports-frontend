8d6f94d272dfedc99842fe88ebea3685cd2a40ac
Syllas Braga <syllasbraga2@gmail.com>
2022-09-30 22:31:51 -0300

    Finalizando a listagem de administradores

--- Visual/assets/script/Administradores.js
+++ Visual/assets/script/Administradores.js
@@ -1,21 +1,49 @@
 const table = document.getElementById("table");
 
-const showData = (result, response) => {
-  for(const campo in result){
+
+function formataData(dataAntiga) {
+  let data = new Date(dataAntiga);
+  let dataFormatada = ((data.getDate())) + "/" +
+    ((data.getMonth() + 1)) + "/" + data.getFullYear();
+  return dataFormatada;
+}
+
+const showData = (result) => {
+  for (let cont = 0; cont < result.length; cont++) {
     var tr = document.createElement("tr");
-    tr.setAttribute("id", "adms-row");
     table.appendChild(tr);
-    for (let i = 0; i < response.length; i++) {
-      var td = document.createElement("td");
-      td.appendChild(response.json()); 
-    }
+    tr.setAttribute("id", "adms-row");
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    var nome = document.createTextNode(result[cont].nome);
+    td.appendChild(nome);
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    var sobreNome = document.createTextNode(result[cont].sobrenome);
+    td.appendChild(sobreNome);
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    var login = document.createTextNode(result[cont].login);
+    td.appendChild(login);
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    var cpf = document.createTextNode(result[cont].cpf);
+    td.appendChild(cpf);
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    var salario = document.createTextNode("R$"+result[cont].salario);
+    td.appendChild(salario);
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    var dataNasc = document.createTextNode(formataData(result[cont].dataNascimento));
+    td.appendChild(dataNasc);
+    tr.appendChild(td);
   }
 }
 
 fetch("http://localhost:8080/administradores")
   .then(response => {
-    response.json()
-    .then(data => showData(data, response));
+    response.json().then(data => showData(data));
   });
 
 
